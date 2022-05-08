ca72a949eb1b8cf60c7c9011d3f639a81b0411be
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-05-09 00:08:13 +0100

    Extended employee service functionality. Added exception handling.

--- src/main/java/com/george/employeemanager/repositoy/EmployeeRepository.java
+++ src/main/java/com/george/employeemanager/repositoy/EmployeeRepository.java
@@ -3,7 +3,12 @@ package com.george.employeemanager.repositoy;
 import com.george.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
 }
--- src/main/java/com/george/employeemanager/service/EmployeeService.java
+++ src/main/java/com/george/employeemanager/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.george.employeemanager.service;
 
+import com.george.employeemanager.exception.UserNotFoundException;
 import com.george.employeemanager.model.Employee;
 import com.george.employeemanager.repositoy.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -18,5 +20,22 @@ public class EmployeeService {
         employee.setJobTitle("Software Engineer");
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + "was not found."));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
     }
 }
